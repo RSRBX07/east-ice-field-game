294a22b737f2e8ddf2bde1a5ceff8c6440f983b7
Pierre Merlin <Pierre Merlin>
2017-01-13 12:35:45 +0100

    add animals

--- game.rb
+++ game.rb
@@ -1,6 +1,7 @@
 # pierre
 bridge = ["pilar", "pilar", "pilar", "pilar", "pilar", "pilar"]
 dice = ["pilar melt", "go to bridge", "go to igloo"]
+animals = ["fox", "bear", "rabbit", "pinguin"]
 # bob
 
 #j
