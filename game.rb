8034b9d31d9a529432edbd8a23cae095141d7b5b
Pierre Merlin <Pierre Merlin>
2017-01-13 12:50:04 +0100

    methods have independant scopes
    fix missing local variable inside method

--- game.rb
+++ game.rb
@@ -3,7 +3,7 @@ bridge = ["pilar", "pilar", "pilar", "pilar", "pilar", "pilar"]
 dice = ["pilar melt", "go to bridge", "go to igloo"]
 animals = ["fox", "bear", "rabbit", "pinguin"]
 
-def ice_melt
+def ice_melt bridge
   bridge.pop
 end
 
