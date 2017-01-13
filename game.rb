c401626e443b6e2bcea0d85d27935ebc4170f5c7
Pierre Merlin <Pierre Merlin>
2017-01-13 13:29:11 +0100

    add board
    close #5

--- game.rb
+++ game.rb
@@ -2,6 +2,7 @@
 bridge = ["pilar", "pilar", "pilar", "pilar", "pilar", "pilar"]
 dice = ["pilar melt", "go to bridge", "go to igloo"]
 animals = ["fox", "bear", "rabbit", "pinguin"]
+board = ["ice field", "bridge","igloo"]
 
 def ice_melt bridge
   puts "Ice is melting"
