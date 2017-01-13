d119495594374f4fc23bc97d76d1951a8ef512ad
Pierre Merlin <Pierre Merlin>
2017-01-13 12:40:16 +0100

    Ice melt
    close #9

--- game.rb
+++ game.rb
@@ -2,6 +2,10 @@
 bridge = ["pilar", "pilar", "pilar", "pilar", "pilar", "pilar"]
 dice = ["pilar melt", "go to bridge", "go to igloo"]
 animals = ["fox", "bear", "rabbit", "pinguin"]
+
+def ice_melt
+  bridge.pop
+end
 # bob
 
 #j
