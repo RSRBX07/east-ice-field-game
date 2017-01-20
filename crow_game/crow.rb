789a8b502d83df2eaeeaf619e043d1fe559d331f
Pierre Merlin <Pierre Merlin>
2017-01-20 12:09:19 +0100

    Fire version of crow

--- crow_game/crow.rb
+++ crow_game/crow.rb
@@ -1,3 +1,21 @@
 class Crow
   attr_reader :advancement
+
+  def walk
+    @advancement += 1
+  end
+
+  def win
+    put "Crow win :-( "
+  end
+
+  def lose
+    puts "Crow lose :-)"
+  end
+
+  private
+
+  def initialize
+    @advancement = 0
+  end
 end
