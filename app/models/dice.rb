a62272f5c150bc40cfb6551ca4304bf5e066b70d
Pierre Merlin <Pierre Merlin>
2017-01-26 23:15:51 +0100

    Dice.roll and init

--- app/models/dice.rb
+++ app/models/dice.rb
@@ -1,3 +1,14 @@
 class Dice < ApplicationRecord
   belongs_to :game
+
+  before_create :roll
+
+  def faces
+    Game::COLORS + [:crow, :basket]
+  end
+
+  def roll
+    self.showing_face = faces.sample
+  end
+
 end
