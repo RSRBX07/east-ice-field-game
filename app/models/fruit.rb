0f9fe14387b93cd413aa3866ee35d83172534dd1
Pierre Merlin <Pierre Merlin>
2017-01-27 00:17:47 +0100

    Orchard.pick_fruit

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,3 +1,19 @@
 class Fruit < ApplicationRecord
   belongs_to :orchard
+
+  def self.on_tree
+    self.where croped_at: nil
+  end
+
+  # Return an ActiveRecord::Relation
+  # if nil is passed as color, it is treated as any
+  # color (it does not scope).
+  def self.of_color color
+    condition = color && {color: color.downcase.to_sym}
+    self.where condition
+  end
+
+  def crop
+    self.update croped_at: DateTime.now
+  end
 end
--- app/models/orchard.rb
+++ app/models/orchard.rb
@@ -4,6 +4,13 @@ class Orchard < ApplicationRecord
 
   before_create :add_fruits
 
+  # Pick a fruit of any given color or
+  # of any color if no argument is given.
+  def pick_fruit color=nil
+    return false unless self.fruits.on_tree.of_color(color).any?
+    self.fruits.on_tree.of_color(color).sample.crop
+  end
+
   private
 
   def add_fruits
@@ -11,4 +18,10 @@ class Orchard < ApplicationRecord
       4.times {fruits << Fruit.new(color: color)}
     end
   end
+
+
+  def fruits_of_color color_or_nil
+    self.fruits.of_color color_or_nil
+  end
+
 end
