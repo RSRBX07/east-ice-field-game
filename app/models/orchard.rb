c32786fa5fbb09d592ab34f293902ce3da9025a9
Pierre Merlin <Pierre Merlin>
2017-01-26 22:57:29 +0100

    add_fruits before create orchard

--- app/models/orchard.rb
+++ app/models/orchard.rb
@@ -1,4 +1,14 @@
 class Orchard < ApplicationRecord
   has_many :fruits
   belongs_to :game
+
+  before_create :add_fruits
+
+  private
+
+  def add_fruits
+    Game::COLORS.each do |color|
+      4.times {fruits << Fruit.new(color: color)}
+    end
+  end
 end
