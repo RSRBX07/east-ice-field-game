c7b6b626db104c4875b961570e5a8090a3ee7ee4
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-01-30 10:27:47 +0100

    button crow

---
+++ app/controllers/crow_controller.rb
@@ -0,0 +1,8 @@
+class CrowController < ApplicationController
+  def step
+    crow = Crow.find params[:id]
+    crow.step
+    crow.save
+    redirect_to game_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'crow/step'
+
   get 'dice/roll'
 
   root 'games#index'
@@ -8,5 +10,7 @@ Rails.application.routes.draw do
   get 'games/:id', to: 'games#show', as: 'game'
 
   post 'dice/:id', to: 'dice#roll', as: 'roll_dice'
+
+  post 'crow/:id', to: 'crow#step', as: 'step_crow'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
