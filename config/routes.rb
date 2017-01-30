8f3e7926e9a07c955bfa9a4e4d95bd4cbf5f58e3
Pierre Merlin <Pierre Merlin>
2017-01-30 10:14:45 +0100

    roll_dice

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-root 'games#index'
+  get 'dice/roll'
+
+  root 'games#index'
   
   get 'games/show'
 
   get 'games/:id', to: 'games#show', as: 'game'
 
+  post 'dice/:id', to: 'dice#roll', as: 'roll_dice'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
