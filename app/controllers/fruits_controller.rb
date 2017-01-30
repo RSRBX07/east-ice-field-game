4ec6e487dd30c732d871188eecb57ccd9c59f221
hind <hind.lepert@cefimis.com>
2017-01-30 11:17:36 +0100

    add if fruit.save

---
+++ app/controllers/fruits_controller.rb
@@ -0,0 +1,18 @@
+class FruitsController < ApplicationController
+  def crop
+    fruit = Fruit.find params[:id]
+    fruit.crop
+    if fruit.save
+      redirect_to game_path(fruit.game)
+    else
+      redirect_to game_path(fruit.game), notice: "Fail to record croped fruit. #{fruit.error.messages}"
+
+  end
+
+  private
+
+  def fruit_params
+
+    params
+  end
+end
