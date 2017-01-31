c760ba5199c7c0bd0c905cf9aea31f0625df6fe8
Your Nameammara <belamm@hotmail.fr>
2017-01-31 09:49:24 +0100

    game_controller avec redirect_to

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,12 @@ class GamesController < ApplicationController
   end
 
   def show
-    @game = Game.find params[:id]
+    begin 
+      # en admettant qu'il n'exise pas de cours dont l'id soit 45
+      @game = Game.find params[:id]
+      Game.find 45
+   rescue
+     redirect_to root_path, notice: "le jeu 45  n'existe pas'"  
+   end 
   end
 end
