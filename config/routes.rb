83d93c4d811aab89ac0745e110810434b8810327
Timy90 <sy.thomas.pro@gmail.com>
2017-01-31 16:07:40 +0100

    add version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'games/new'
+
+  get 'games/create'
+
   root 'games#index'
   
   get 'games/show'
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,22 +2,16 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #new" do
     it "returns http success" do
-      get :index
+      get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-<<<<<<< HEAD
-  describe "GET #game" do
+  describe "GET #create" do
     it "returns http success" do
-      get :game
-=======
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
->>>>>>> 7cad6582ead59a81b777bb1b020d807c671b50bb
+      get :create
       expect(response).to have_http_status(:success)
     end
   end
