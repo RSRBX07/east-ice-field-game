a9d885c9e19c24ff4762ce7a381e2b88c8863ac6
Timy90 <sy.thomas.pro@gmail.com>
2017-01-31 11:56:30 +0100

    add version
    Fruits controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'crow/step'
-
-  get 'dice/roll'
-
   root 'games#index'
   
   get 'games/show'
@@ -15,6 +11,8 @@ Rails.application.routes.draw do
   post 'dice/:id', to: 'dice#roll', as: 'roll_dice'
 
   post 'crow/:id', to: 'crow#step', as: 'step_crow'
+
+  post 'crop/:id', to: 'fruits#crop', as: 'crop_fruit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
