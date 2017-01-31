8f7da0b4d0b648597038d95c12d595e48b0d348e
Your Nameammara <belamm@hotmail.fr>
2017-01-31 14:01:17 +0100

    game avec partials

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,16 +4,21 @@ class GamesController < ApplicationController
   end
 
   def show
-    begin 
-      # en admettant qu'il n'exise pas de cours dont l'id soit 45
+    
       @game = Game.find params[:id]
-      Game.find 45
-   rescue
-     redirect_to root_path, notice: "le jeu 45  n'existe pas'" # ou bien ecrire cela: 
      
-     # alert: "le jeu #{params[:id]}n'existe pas"
-     # notice: "vous avez ete rederiger vers l'acceuil du jeu"
-     # mais modifier sur application.html.erb comme suite : 
-   end 
   end
 end
+
+
+#  begin 
+#       # en admettant qu'il n'exise pas de cours dont l'id soit 45
+#       @game = Game.find params[:id]
+#       Game.find 45
+#    rescue
+#      redirect_to root_path, notice: "le jeu 45  n'existe pas'" # ou bien ecrire cela: 
+     
+#      # alert: "le jeu #{params[:id]}n'existe pas"
+#      # notice: "vous avez ete rederiger vers l'acceuil du jeu"
+#      # mais modifier sur application.html.erb comme suite : 
+#    end 
