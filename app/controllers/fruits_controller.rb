2cecc0190add7161e9c783da24adf580cb7cc466
hind <hind.lepert@cefimis.com>
2017-02-06 11:57:30 +0100

    validate methode crop

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -2,11 +2,15 @@ class FruitsController < ApplicationController
 
   def crop
     fruit = Fruit.find fruit_params[:id]
-    fruit.crop
-    if fruit.save
-      redirect_to game_path(fruit.game)
-    else
-      redirect_to game_path(fruit.game), notice: "#{fruit.error.messages}"
+    # valider la methode en verifiant le fruit cropé en fonction de la face du dé
+    showing_face = fruit.game.dice.showing_face
+    if showing_face == fruit.color
+      fruit.crop
+      if fruit.save
+        redirect_to game_path(fruit.game)
+      else
+        redirect_to game_path(fruit.game), notice: "#{fruit.error.messages}"
+      end
     end
   end
 
