e01c7d88baf29cfd8dad7ef669abda04b1ae1289
Pierre Merlin <Pierre Merlin>
2017-02-09 23:44:56 +0100

    fix spec close #60

--- spec/models/dice_spec.rb
+++ spec/models/dice_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Dice, type: :model do
   let(:dice) { build :dice }
   describe "Dice#roll" do
     it "change showing face" do
-      expect{ dice.roll }.to change{ dice.showing_face }
+      allow(dice).to receive(:faces).and_return([42])
+      expect{ dice.roll }.to change{ dice.showing_face }.to "42"
     end
   end
 end
