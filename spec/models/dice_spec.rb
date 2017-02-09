b10da00d057a80d3c7500a012c4a428d351b4874
hind <hind.lepert@cefimis.com>
2017-02-09 09:57:37 +0100

    fix  rolling dice on crop
    close #54

--- spec/models/dice_spec.rb
+++ spec/models/dice_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Dice, type: :model do
   let(:dice) { build :dice }
-  it "is initalized with a showing face" do
-    expect(dice.showing_face).not_to be_nil
-  end
   describe "Dice#roll" do
     it "change showing face" do
       expect{ dice.roll }.to change{ dice.showing_face }
