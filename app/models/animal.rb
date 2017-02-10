a8199c8d1aaeeab11f40dea14eba58f668227aad
hind <hind.lepert@cefimis.com>
2017-02-10 15:53:58 +0100

    update animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,9 @@
 class Animal < ApplicationRecord
   belongs_to :ice_field_game
+  validates :name, presence: true, inclusion: IceFieldGame::ANIMALS.map{|e| e.to_s}
+  validates :place, presence: true, inclusion: IceFieldGame::PLACES.map{|e| e.to_s}
+  
+  def go_to place
+    self.place = place
+  end
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Animal, type: :model do
   it "is named fox, bear, rabit or pinguin"
   it "is placed on ice_field, bridge or igloo"
   it "is created on ice_field"
-  describe "Animal#step" do
+  describe "Animal#go_to" do
     it "go from ice_field to bridge"
     it "go from bridge to igloo"
   end
